752dc5b8f589afd9aacfce28b67c24ed7ea21a86
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2022-10-18 02:26:50 -0400

    getting started on player movement

---
+++ Screamo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
